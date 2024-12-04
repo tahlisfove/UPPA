cb708085e00cde326ffd1e903a6eb1ce4acb8a14
Christoph Samuel <129196750+tahlisfove@users.noreply.github.com>
2024-12-04 16:33:51 +0100

    Delete master_informatique/m2_informatique/cloud_infrastructure directory

--- master_informatique/m2_informatique/cloud_infrastructure/budget-app/login-frontend/public/script.js
+++
@@ -1,4 +0,0 @@
-// Sélectionne l'élément avec l'identifiant 'loginButton' dans le DOM
-document.getElementById('loginButton').addEventListener('click', () => {
-    window.location.href = 'http://localhost:3002/auth/google';
-});
